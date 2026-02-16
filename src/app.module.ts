fdb930ac8376ea1b6429f774b9e0326e283dd797
Guilherme Lima Desoler <guilherme.desoler@gmail.com>
2026-02-16 18:47:34 -0300

    feat: create auth and firebase module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseModule } from './firebase/firebase.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [FirebaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
